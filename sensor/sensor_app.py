6bd02321adfb05dfb9f6f746ac0afda2de98679a
Amit Chaurasia <xxammuxx@gmail.com>
2022-08-30 22:40:45 +0530

    commit 1

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,19 +1,16 @@
 # Runner script for all modules
+from load_data import load_sensor_data          # module 1
+   
 
-
-##############################
+#######################################
 # Do not remove these two lines
-# They are needed to validate your unittest
-data = []
+# if you do, it will break the unittest
+data = []                   
 print("Sensor Data App")
-##############################
+#######################################
 
-# Module 1 code here:
+# YOUR CODE HERE
 
-# Module 2 code here:
-
-# Module 3 code here:
-
-# Module 4 code here:
-
-# Module 5 code here:
+# Module 1
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
